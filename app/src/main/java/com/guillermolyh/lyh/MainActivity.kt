a8153feefcf7c035d82ea5c719dacad1944cf8fe
GuillermoLyH <guillermolyh@gmail.com>
2020-08-08 16:25:38 -0600

    initial commit

---
+++ app/src/main/java/com/guillermolyh/lyh/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.guillermolyh.lyh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this,getString(R.string.welcome),Toast.LENGTH_SHORT).show()
+    }
+}
