a7919679f06132d69893248465d786456ae95228
GuillermoLyH <guillermolyh@gmail.com>
2020-10-14 00:11:51 -0600

    New design

--- app/src/main/java/com/guillermolyh/lyh/HomeFragment.kt
+++ app/src/main/java/com/guillermolyh/lyh/HomeFragment.kt
@@ -29,31 +29,31 @@ class HomeFragment : Fragment() {
 
         }
         //Fragmento capacitaciones
-        binding.imageView4.setOnClickListener { view: View ->
+        binding.imageView10.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_homeFragment_to_capacitacionesFragment)
 
         }
 
         //Fragmento compras
-        binding.imageView8.setOnClickListener { view: View ->
+        binding.imageView11.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_homeFragment_to_comprasFragment)
 
         }
 
 
         //Fragmento consultorias
-        binding.imageView9.setOnClickListener { view: View ->
+        binding.imageView8.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_homeFragment_to_consultoriaFragment)
 
         }
 
         //Fragmento asesorias
-        binding.imageView11.setOnClickListener { view: View ->
+        binding.imageView4.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_homeFragment_to_asesoriaFragment)
 
         }
         //Fragmento judicial
-        binding.imageView10.setOnClickListener { view: View ->
+        binding.imageView9.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_homeFragment_to_judicialFragment)
 
         }
--- app/src/main/java/com/guillermolyh/lyh/MainActivity.kt
+++ app/src/main/java/com/guillermolyh/lyh/MainActivity.kt
@@ -27,7 +27,6 @@ class MainActivity : AppCompatActivity() {
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
 
 
-        Toast.makeText(this,getString(R.string.welcome),Toast.LENGTH_SHORT).show()
     }
 
     override fun onSupportNavigateUp(): Boolean {
